05957252a58a96ac10324cbda175f93cd471aca8
fbraz-aa <fbraz@appannie.com>
2017-02-03 10:11:47 -0800

    First commit

---
+++ RealmVsSQLite/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataGenerator.swift
+//  RealmVsSQLite
+//
+//  Created by Felipe P Braz on 2017-02-03.
+//  Copyright © 2017 iOS Quickstart. All rights reserved.
+//
+
+import Foundation
+
+struct DataGenerator {
+    
+    func generate
+    
+    
+}
---
+++ RealmVsSQLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmVsSQLite
+//
+//  Created by Felipe P Braz on 2017-01-30.
+//  Copyright © 2017 iOS Quickstart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
